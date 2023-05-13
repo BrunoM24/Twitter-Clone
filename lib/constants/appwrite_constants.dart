aff3de51aad1bba7b614f68715b5ae7280e9d67c
BrunoM24 <brunocostamatos.24@gmail.com>
2023-05-13 22:37:24 +0100

    Added the Appwrite dependency

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const projectId = "645fb2767f2b41cf9267";
+  static const dbId = "646001e5ea757958800c";
+  static const endPoint = "https://cloud.appwrite.io/v1";
+}
