86e750d558bd8234db26c53dfb202100b3291f4a
BrunoM24 <brunocostamatos.24@gmail.com>
2023-05-15 10:14:27 +0100

    Added the flutter_svg dependency and the AppBar

---
+++ lib/features/auth/view/login_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:twitter_clone/constants/constants.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final appBar = UIConstants.appBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar,
+    );
+  }
+}
