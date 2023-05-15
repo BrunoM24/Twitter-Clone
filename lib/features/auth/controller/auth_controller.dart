3145c7150e2c04c0c15304e06f952c60e7a59aef
BrunoM24 <brunocostamatos.24@gmail.com>
2023-05-15 15:44:24 +0100

    Implemented the authController

---
+++ lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/apis/auth_api.dart';
+import 'package:twitter_clone/core/utils.dart';
+
+final authControllerProvider = StateNotifierProvider<AuthController, bool>(
+  (ref) => AuthController(
+    authAPI: ref.watch(authAPIProvider),
+  ),
+);
+
+class AuthController extends StateNotifier<bool> {
+  final AuthAPI _authApi;
+
+  AuthController({required AuthAPI authAPI})
+      : _authApi = authAPI,
+        super(false);
+
+  Future<void> signup({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    state = true;
+
+    final response = await _authApi.signup(email: email, password: password);
+
+    response.fold(
+      (failure) => showSnackBar(context, failure.message),
+      (account) => print(account.name),
+    );
+  }
+}
